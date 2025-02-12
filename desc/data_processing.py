6556a5c96cc879d8c47b8e6e06bda9fc66d90f86
Quantum Chemistry Group - Universitat Rovira i Virgili <162990336+qcgurv@users.noreply.github.com>
2025-02-12 20:35:04 +0100

    Update data_processing.py

--- desc/data_processing.py
+++ desc/data_processing.py
@@ -1,12 +1,19 @@
 import os
 import sys
 import pandas as pd
-from .file_processing import parse_job, process_file
+import threading
+import numpy as np
+from .file_processing import parse_job, count_frames, process_file
+from .utils import progress_bar
+
+import time
+import cProfile
+import pstats
 
 def data_processing(job_file):
     print("  Reading Input File")
 
-    ## Check if input is provided
+    # Check if input is provided
     if len(sys.argv) != 2:
         print("No input file.")
         print("Usage: desc.py <job input>")
@@ -25,21 +32,28 @@ def data_processing(job_file):
 
     ## Process input file
     print("  Reading PDB Trajectory File")
-    frame_data, total_frames, box_length = process_file(traj, frames)
+    total_frames, frame_size, box_length = count_frames(traj)
+
+    current_frame = [0]  # Mutable object to track current frame
+    progress_thread = threading.Thread(target=progress_bar, args=(current_frame, total_frames, '  Processing File:', 'Complete', 1, 50, '█'))
+    progress_thread.start()
+
+    frame_data, current_frame_value, box_length = process_file(traj, frames, total_frames, box_length, frame_size, current_frame)
+
     all_frame_data = [line for frame in frame_data for line in frame_data[frame]]
 
-    maindf_datatypes = {
-        "frame": "int32",
-        "index": "int32",
-        "atom": "category",
-        "residue": "category",
-        "residue_number": "int32",
-        "x": "float32",
-        "y": "float32",
-        "z": "float32"
-        }
+    frame_data_array = np.array(all_frame_data, dtype=object)
 
-    maindf = pd.DataFrame(all_frame_data, columns=list(maindf_datatypes.keys())).astype(maindf_datatypes)
+    maindf = pd.DataFrame({
+        "frame": frame_data_array[:, 0].astype(np.int32),
+        "index": frame_data_array[:, 1].astype(np.int32) if np.issubdtype(frame_data_array[:, 1].dtype, np.number) else frame_data_array[:, 1].astype(str),
+        "atom": pd.Categorical(frame_data_array[:, 2]),
+        "residue": pd.Categorical(frame_data_array[:, 3]),
+        "residue_number": frame_data_array[:, 4].astype(np.int32) if np.issubdtype(frame_data_array[:, 4].dtype, np.number) else frame_data_array[:, 4].astype(str),
+        "x": frame_data_array[:, 5].astype(np.float32),
+        "y": frame_data_array[:, 6].astype(np.float32),
+        "z": frame_data_array[:, 7].astype(np.float32)
+    })
 
     # Create and move into directory
     base_name = "DESC_JOB"
@@ -57,5 +71,5 @@ def data_processing(job_file):
     ## MD Trajectory Data
     natoms_ss = maindf["index"].nunique()
     filt_ss = len(maindf) / natoms_ss
-
-    return maindf, maindir, natoms_ss, filt_ss, box_length, ref, atname, resname, qform
+   
+    return maindf, maindir, box_length, ref, atname, resname, qform
